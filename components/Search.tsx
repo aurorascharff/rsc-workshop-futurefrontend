4983c50129529cbb672a90eba19414603cd40f77
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-26 11:55:42 +0300

    Add spinner to search

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,17 +1,23 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import React from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useTransition } from 'react';
 import { SearchIcon, SpinnerIcon } from './ui/icons';
 
 export default function Search() {
-  const searching = false;
   const searchParams = useSearchParams();
   const q = searchParams.get('q') || '';
+  const router = useRouter();
+  const [searching, startTransition] = useTransition();
 
   return (
     <form role="search">
       <input
+        onChange={e => {
+          startTransition(() => {
+            router.push(`?q=${e.target.value}`);
+          });
+        }}
         className="w-full pl-8 outline-offset-1"
         aria-label="Search contacts"
         name="q"
