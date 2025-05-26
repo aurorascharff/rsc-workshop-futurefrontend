bbbab7eefd41bbbe231226e12b71ade43e77b327
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-26 14:14:20 +0300

    Add Create in CRUD

---
+++ data/actions/createEmptyContact.ts
@@ -0,0 +1,13 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+import { prisma } from '@/db';
+
+export async function createEmptyContact() {
+  const contact = await prisma.contact.create({
+    data: {},
+  });
+
+  //   revalidatePath('/');
+  redirect(`/contacts/${contact.id}/edit`);
+}
