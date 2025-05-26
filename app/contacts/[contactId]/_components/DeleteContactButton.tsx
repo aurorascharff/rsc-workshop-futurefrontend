2cc54af3b3154eee748143a75282ad59a2c7c354
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-26 15:10:42 +0300

    Implement delete with confirm modal

---
+++ app/contacts/[contactId]/_components/DeleteContactButton.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React from 'react';
+import Button from '@/components/ui/Button';
+import { deleteContact } from '@/data/actions/deleteContact';
+
+type Props = {
+  contactId: string;
+};
+
+export default function DeleteContactButton({ contactId }: Props) {
+  return (
+    <form
+      action={async () => {
+        const response = confirm('Are you sure you want to delete this contact? This action cannot be undone.');
+        if (response) {
+          await deleteContact(contactId);
+        }
+      }}
+    >
+      <Button type="submit" theme="destroy">
+        Delete
+      </Button>
+    </form>
+  );
+}
