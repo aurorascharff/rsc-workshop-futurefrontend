7adccab7d244469ab7b299ba2833ae1023290906
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-26 11:12:08 +0300

    Add global notfound boundary

---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 Not found</h1>
+      <span>
+        Sorry, we couldnt find the page you were looking for. It may have been removed, renamed, or did not exist in the
+        first place.
+      </span>
+    </div>
+  );
+}
