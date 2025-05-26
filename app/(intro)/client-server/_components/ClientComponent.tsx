6fed3b67c7c4d538e6601ca6da0768be21b779c1
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-26 14:02:35 +0300

    Add intro to server functions

--- app/(intro)/client-server/_components/ClientComponent.tsx
+++ app/(intro)/client-server/_components/ClientComponent.tsx
@@ -5,17 +5,19 @@ import React from 'react';
 type Props = {
   children: React.ReactNode;
   content: React.ReactNode;
+  mutateData: () => Promise<string>;
 };
 
-export default function ClientComponent({ children, content }: Props) {
+export default function ClientComponent({ mutateData, children, content }: Props) {
   // Without "use client" we cannot use onClick, useEffect, useState, etc.
   return (
     <div className="rounded border-2 border-blue-500 p-4">
       {children}
       {content}
       <button
-        onClick={() => {
-          return alert('This button does nothing!');
+        onClick={async () => {
+          const result = await mutateData();
+          return alert(result);
         }}
       >
         Click me
--- app/(intro)/client-server/page.tsx
+++ app/(intro)/client-server/page.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { prisma } from '@/db';
 import ClientComponent from './_components/ClientComponent';
 import ServerComponent from './_components/ServerComponent';
 
 export default function ClientServerPage() {
+  // This function will have a hidden API endpoint created and can be called from the client
+  async function mutateData() {
+    'use server';
+
+    const result = await prisma.contact.findMany();
+    return result[0].first as string;
+  }
+
   return (
     <div>
       ClientServerPage
-      <ClientComponent content={<ServerComponent />}>
+      <ClientComponent mutateData={mutateData} content={<ServerComponent />}>
         <ServerComponent />
       </ClientComponent>
     </div>
