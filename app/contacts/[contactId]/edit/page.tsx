801ac04adc9e5016d71c610127b6088d6d7d5044
Aurora Scharff <aurora.sofie@gmail.com>
2025-05-26 13:35:18 +0300

    Add loading fallbavk for edit page

--- app/contacts/[contactId]/edit/page.tsx
+++ app/contacts/[contactId]/edit/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+import Skeleton from '@/components/ui/Skeleton';
 import ContactForm from './_components/ContactForm';
 
 type PageProps = {
@@ -8,5 +10,9 @@ type PageProps = {
 
 export default async function EditContactPage({ params }: PageProps) {
   const contactId = (await params).contactId;
-  return <ContactForm contactId={contactId} />;
+  return (
+    <Suspense fallback={<Skeleton />}>
+      <ContactForm contactId={contactId} />;
+    </Suspense>
+  );
 }
